ebef507c38c0dcf45f993b99dbbd4c1ab8fe7ba2
Kubra Alp <kubra.alp.k@gmail.com>
2024-07-16 23:24:38 +0300

    redux installation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customPink: "#E92577",
+        customBlue: "#DDEEFE",
+        customGreen: "#D9F6F1",
+        customBlack: "#0A0A14",
+      },
+      fontFamily: {
+        sans: ["Inter,  sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
